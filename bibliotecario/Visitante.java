0f36fc9e43b590db3b702067f2c7bb84794a49e3
Lírio Manga <147447200+LirioManga@users.noreply.github.com>
2024-03-18 09:32:31 +0200

    Buscando livros na BD e preenchendo a tabela

--- bibliotecario/Visitante.java
+++ bibliotecario/Visitante.java
@@ -2,161 +2,72 @@ package bibliotecario;
 import bibliotecario.HomeBiblioteca;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+public class Visitante extends JFrame {
+    private DefaultTableModel modelo;
+    private JTable tabela;
 
-public class Visitante extends JFrame implements ActionListener{
-	private String name;
-	private String password;
-	
-	// setters & getters
-	public void setNome(){
-		this.name = "Lirio";
-	}
-	
-	public String getNome(){
-		return name;
-	}
-	
-	public void setSenha(){
-		this.password = "lirio10";
-	}
-	
-	public String getSenha(){
-		return password;
-	}
-	
-	// Frame
-	JPanel login;
-	JPanel painel;
-	
+    public Visitante() {
+        
+        modelo = new DefaultTableModel();
+        tabela = new JTable(modelo);
 
-	JLabel labelTitulo,labelNome,labelInst, nrLabel;
-	JTextField fieldNome, fieldnr,fieldInst;
-	JButton buttonLogin;
-	
-	public Visitante(){
-		setNome();
-		setSenha();
-		
-		login = new JPanel();
-		login.setBounds(130,90,400,300);
-		login.setLayout(null);
-		login.setBackground(new Color(0x123456));
-		
-		labelTitulo = new JLabel("Dados Visitante");
-		labelTitulo.setBounds(90, 20, 300, 30);
-		labelTitulo.setFont(new Font("mv boli", Font.BOLD, 30));
-		labelTitulo.setForeground(Color.white);
-		
+     
+        modelo.addColumn("ID");
+        modelo.addColumn("Autor");
+        modelo.addColumn("Titulo");
+        modelo.addColumn("Editora");
+        modelo.addColumn("Categoria");
 
-		nrLabel = new JLabel("Nr_Visitante");
-		nrLabel.setBounds(100, 60, 80, 25);
-		nrLabel.setForeground(Color.white);
+      
+        JScrollPane scrollPane = new JScrollPane(tabela);
+        scrollPane.setBounds(10, 10, 500, 200);
+        add(scrollPane);
 
-		fieldnr = new JTextField();
-		fieldnr.setBounds(175, 60, 130, 25);
+      
+        setTitle("Livros");
+        setSize(700, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+        setVisible(true);
 
-		labelNome = new JLabel("Nome");
-		labelNome.setBounds(100, 120, 50, 25);
-		labelNome.setForeground(Color.white);
-		
-		fieldNome = new JTextField();
-		fieldNome.setBounds(140, 120, 130, 25);
-		
-		labelInst = new JLabel("Instituicao");
-		labelInst.setBounds(100, 180, 100, 25);
-		labelInst.setForeground(Color.white);
-		
-		fieldInst = new JTextField();
-		fieldInst.setBounds(180,180,130,25);
-		
-		
-		buttonLogin = new JButton("Registar");
-		buttonLogin.setFont(new Font("mv boli", Font.ITALIC, 15));
-		buttonLogin.setBounds(160, 250, 100, 30);
-		buttonLogin.setFocusable(false);
-		buttonLogin.addActionListener(this);
-		
-		// adicionando elementos ao panel
-		login.add(nrLabel);
-		login.add(fieldnr);
-		login.add(labelTitulo);
-		login.add(labelNome);
-		login.add(fieldNome);
-		login.add(labelInst);
-		login.add(fieldInst);
-		login.add(buttonLogin);
-		
-			
-		// frame settings
-		
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setTitle("Visitante");
-		this.setLayout(null);
-		this.setSize(700,500);
-		this.setVisible(true);
-		add(login);
-		
-	}
-	
-	
-	@Override
-	public void actionPerformed(ActionEvent e){
-			
-		if(e.getSource() == buttonLogin){
-			String nr_visitante = fieldnr.getText();
-			String nome = fieldNome.getText();
-			String instituicao = fieldInst.getText();
-			
-				if(!nome.isEmpty()){
-				
-				String url = "jdbc:mysql://localhost:3306/biblioteca";
-				String usuario = "root";
-				String senha = "";
-				
-				try{
-					Class.forName("com.mysql.cj.jdbc.Driver");
-					 Connection conexao = DriverManager.getConnection(url, usuario, senha);
-					
-					String sql = "INSERT INTO visitantes (nr_id,nome,instituicao) VALUES (?,?,?)";
-					
-					try (PreparedStatement preparedStatement = conexao.prepareStatement(sql)) {
-                
-					preparedStatement.setString(1,nr_visitante);
-					preparedStatement.setString(2,nome);
-					preparedStatement.setString(3,instituicao);
-			
+        
+        preencherTabela();
+    }
 
-				
-					preparedStatement.executeUpdate();
+    
+    private void preencherTabela() {
+        String url = "jdbc:mysql://localhost:3306/biblioteca";
+        String usuario = "root";
+        String senha = "";
 
-					System.out.println("Dados inseridos com sucesso.");
-				}
-					conexao.close();
-					
-					
-				}catch (ClassNotFoundException f){
-					
-					f.printStackTrace();
-					
-				}catch(SQLException f){
-					
-					f.printStackTrace();
-				}
-			}
-			
-			dispose();
-			new ShowLivros();
-		
-		}
-		
-	}
-	
-	
+        try {
+            Connection conexao = DriverManager.getConnection(url, usuario, senha);
+            String sql = "SELECT * FROM livros";
+
+            PreparedStatement preparedStatement = conexao.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String autor = resultSet.getString("autor");
+                String titulo = resultSet.getString("titulo");
+                String editora = resultSet.getString("editora");
+                String categoria = resultSet.getString("categoria");
+
+                modelo.addRow(new Object[]{autor, titulo, editora, categoria});
+            }
+
+            conexao.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
